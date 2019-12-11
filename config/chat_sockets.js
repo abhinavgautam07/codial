e5e79d065489c2aa4b07a16f874464874b4254e8
abhinav gautam <abhinavgautam36@gmail.com>
2019-12-12 00:24:15 +0530

    complete chat part

--- config/chat_sockets.js
+++ config/chat_sockets.js
@@ -2,6 +2,26 @@
 // it is going to receive connections from all the subscriber.
 // user that is the subscriber initiates the connection, the observer detects the 
 // connection and then acknowledges it
-module.exports.chatSockets = function(chatServer) {
+//here it receives thr request from frontend 
+module.exports.chatSockets = function(socketServer) {
+    let io = require('socket.io')(socketServer);
+    //the connection is established the socket is sent back
+
+    io.sockets.on('connection', function(socket) {
+        console.log("new connection established", socket.id);
+
+        socket.on('disconnect', function() {
+            console.log("sockets disconnected");
+        });
+        socket.on('join_room', function(data) {
+            console.log('joining request rec.', data);
+            socket.join(data.chatroom);
+            io.in(data.chatroom).emit('user_joined', data);
+        });
+        socket.on('send_message', function(data) {
+
+            io.in(data.chatroom).emit('receive_message', data);
+        });
+    });
 
 }
